a6b4f454bdfe07b39191b859573f7b7815f37fa7
mysersan <60083537+mysersan@users.noreply.github.com>
2020-02-21 11:59:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -56,6 +56,16 @@ $app->post('/', function ($request, $response)
 		}
 		
 		$userMessage = $event['message']['text'];
+		elseif(strtolower($userMessage) != 'hai')
+		{
+			$message = "Halo Halo Hai";
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		
+		}
+		
+		$userMessage = $event['message']['text'];
 		if(strtolower($userMessage) == "gambar")
 		{
 			$imageMessage = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder("https://akcdn.detik.net.id/community/media/visual/2019/08/14/273a412d-33c2-46be-b2a9-4ee033fa1217_43.jpeg?w=700&q=90","https://akcdn.detik.net.id/community/media/visual/2019/08/14/273a412d-33c2-46be-b2a9-4ee033fa1217_43.jpeg?w=700&q=90");
@@ -63,21 +73,7 @@ $app->post('/', function ($request, $response)
 			return $result->getHTTPStatus() . ' ' . $result->getRawBody();     
 		}
 		
-		$userMessage = $event['message']['text'];
-		if(strtolower($userMessage == "cs")
-		{
-		$carouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder([
-		  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("title", "text","https://i0.wp.com/angryanimebitches.com/wp-content/uploads/2013/03/tamakomarket-overallreview-tamakoanddera.jpg",[
-		  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('buka',"http://hilite.me/"),
-		  ]),
-		  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("title", "text","https://i0.wp.com/angryanimebitches.com/wp-content/uploads/2013/03/tamakomarket-overallreview-tamakoanddera.jpg",[
-		  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka',"http://hilite.me/"),
-		  ]),
-		  ]);
-		$templateMessage = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('nama template',$carouselTemplateBuilder);
-		$result = $bot->replyMessage($event['replyToken'], $templateMessage);
-		return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-		}
+		
 			
 	}
 	
