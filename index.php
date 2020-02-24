ef8c2ec55c56b0bfd023fa14a08584829e70483e
mysersan <60083537+mysersan@users.noreply.github.com>
2020-02-24 10:26:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -77,13 +77,13 @@ $app->post('/', function ($request, $response)
 			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/cloud_developer_logo_201219145056.png",
 			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Cloud Developer',"")),
 			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/web_developer_logo_201219135331.png",
-			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Front-End Web Developer',"")),
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Front-End Web Dev',"")),
 			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/ar_vr_developer_logo_301219145216.png",
 			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('AR/VR Developer',"")),
 			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/unity_game_developer_logo_201219135320.png",
-			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Unity Game Developer',"")),
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Unity Game Devel',"")),
 			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/construct_game_developer_logo_201219135236.png",
-			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Construct Game Developer',"")),
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Construct Game Dev',"")),
 
 			  ]);
 			$templateMessage = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('nama template',$ImageCarouselTemplateBuilder);
