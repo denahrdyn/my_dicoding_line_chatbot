05f79fdd85403776f74598ac890c902f912f8618
mysersan <60083537+mysersan@users.noreply.github.com>
2020-02-24 09:39:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -68,12 +68,21 @@ $app->post('/', function ($request, $response)
 		}
 			
 		$userMessage = $event['message']['text'];
-		if(strtolower($userMessage) == "patner"){
+		if(strtolower($userMessage) == "learning path"){
 			$ImageCarouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder([
-			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/commons/partner-aws-educate.jpg",
-			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('AWS',"https://d2zvxgfo5lha7k.cloudfront.net/original/commons/partner-aws-educate.jpg")),
-			   new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/commons/partner-line.jpg",
-			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('LINE',"https://d2zvxgfo5lha7k.cloudfront.net/original/commons/partner-line.jpg")),
+			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/android_developer_logo_201219145044.png",
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Android Developer',"https://www.dicoding.com/learningpaths/7")),
+			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/cloud_developer_logo_201219145056.png",
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Cloud Developer',"https://d2zvxgfo5lha7k.cloudfront.net/original/commons/partner-line.jpg")),
+			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/web_developer_logo_201219135331.png",
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Front-End Web Developer',"https://d2zvxgfo5lha7k.cloudfront.net/original/commons/partner-line.jpg")),
+			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/ar_vr_developer_logo_301219145216.png",
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('AR/VR Developer',"https://d2zvxgfo5lha7k.cloudfront.net/original/commons/partner-line.jpg")),
+			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/unity_game_developer_logo_201219135320.png",
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Unity Game Developer',"https://d2zvxgfo5lha7k.cloudfront.net/original/commons/partner-line.jpg")),
+			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder("https://d2zvxgfo5lha7k.cloudfront.net/original/academy/construct_game_developer_logo_201219135236.png",
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Construct Game Developer',"https://d2zvxgfo5lha7k.cloudfront.net/original/commons/partner-line.jpg")),
+			  
 			  ]);
 			$templateMessage = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('nama template',$ImageCarouselTemplateBuilder);
 			$result = $bot->replyMessage($event['replyToken'], $templateMessage);
@@ -81,13 +90,19 @@ $app->post('/', function ($request, $response)
 		}	
 			
 		$userMessage = $event['message']['text'];
-		if(strtolower($userMessage) == "cc"){
+		if(strtolower($userMessage) == "lp android"){
 			$carouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder([
+			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Memulai Pemrograman Dengan Kotlin", "Disusun oleh: Dicoding Indonesia","https://d2zvxgfo5lha7k.cloudfront.net/original/academy/memulai_pemrograman_dengan_kotlin_logo_071119141033.png",[
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('buka',"https://www.dicoding.com/academies/80"),
+			  ]),
+			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Belajar Membuat Aplikasi Android untuk Pemula", "Disusun oleh: Google ATP","https://d2zvxgfo5lha7k.cloudfront.net/original/academy/belajar_membuat_aplikasi_android_untuk_pemula_logo_071119140631.png",[
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka',"https://www.dicoding.com/academies/51"),
+			  ]),
+			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Belajar Fundamental Aplikasi Android", "Disusun oleh: Google ATP","https://d2zvxgfo5lha7k.cloudfront.net/original/academy/menjadi_android_developer_expert_logo_071119140536.png",[
+			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('buka',"https://www.dicoding.com/academies/14"),
+			  ]),
 			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Belajar Android Jetpack Pro", "Disusun oleh: Google ATP","https://d2zvxgfo5lha7k.cloudfront.net/original/academy/menjadi_android_developer_expert_logo_071119140536.png",[
 			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('buka',"https://www.dicoding.com/academies/129"),
-			  ]),
-			  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("title", "text","https://i0.wp.com/angryanimebitches.com/wp-content/uploads/2013/03/tamakomarket-overallreview-tamakoanddera.jpg",[
-			  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Buka',"http://hilite.me/"),
 			  ]),
 			  ]);
 			$templateMessage = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('nama template',$carouselTemplateBuilder);
