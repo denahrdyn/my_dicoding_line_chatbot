3e0faecb2206613ce750150d4e3806c10ed7715a
mysersan <60083537+mysersan@users.noreply.github.com>
2020-02-24 14:49:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -71,15 +71,6 @@ $app->post('/', function ($request, $response)
 		
 		}
 		
-		$userMessage = $event['message']['text'];
-		if(strtolower($userMessage) != 'hai')
-		{
-			$message = "maaf kaka, kata kunci belum terdaftar.";
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
-			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-		
-		}
 		
 		$userMessage = $event['message']['text'];
 		if(strtolower($userMessage) == "ceo"){
